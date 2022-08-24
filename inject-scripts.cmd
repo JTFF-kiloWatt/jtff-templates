638dac79941c61b08be908d8cdfa2133e8ff05be
Fabian Ingremeau <fabian.ingremeau@outlook.com>
2022-08-24 11:44:28 +0200

    feat(injector): Add file selector window and dependency checking

--- inject-scripts.cmd
+++ inject-scripts.cmd
@@ -1,5 +1,58 @@
-@echo off
-set /P mizfile=Enter .miz File:
-echo %mizfile%
-npm run jtff-inject --mission=%mizfile%
+<# : powershell-exec.bat
+    :: Start batch
+    @echo off
+    setlocal
+    powershell -ExecutionPolicy RemoteSigned -Command "Invoke-Expression $([System.IO.File]::ReadAllText('%~f0'))"
+    endlocal
+    goto :eof
+    :: End batch
+#>
+
+function CheckDependencies()
+{
+    # Check npm installed
+    $command = npm --version
+    if(!$?)
+    {
+        Write-Host "ERROR: npm is not installed. Please install npm and try again." -ForegroundColor Red
+        Exit
+    }
+
+    # Check node_modules installed
+    $path = (Get-Item .).FullName + "\node_modules"
+    if (![System.IO.Directory]::Exists($path))
+    {
+        Write-Host "ERROR: node_modules folder does not exist. Please run 'npm install' and try again." -ForegroundColor Red
+        Exit
+    }
+}
+
+function GetFile() {
+    Add-Type -AssemblyName System.Windows.Forms
+    $f = new-object Windows.Forms.OpenFileDialog
+    $f.InitialDirectory = pwd
+    $f.Filter = "Mission Files (*.miz)|*.miz"
+    $f.Multiselect = $false
+    [void]$f.ShowDialog()
+    return $f.FileName
+}
+
+function Main()
+{
+    CheckDependencies
+
+    $file = GetFile
+    if ($file -eq "")
+    {
+        Write-Output("No file selected. Exiting...")
+        return
+    }
+
+    $command = "npm run jtff-inject --mission=$file"
+    Write-Output("Injecting scripts in: $file")
+    Invoke-Expression $command
+}
+
+Main
+
 pause
