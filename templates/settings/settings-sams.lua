3486f6944b7b63ff96fd03e06ef539bb17df38e0
Sebastien LONGO <sebastien.longo@oufy.net>
2022-08-26 14:54:25 +0200

    feat(inject-scripts): add 191-sams scripts support

---
+++ templates/settings/settings-sams.lua
@@ -0,0 +1,19 @@
+SamsConfig = {
+    {
+        name = "Akamas",
+        enable = false,
+        benefit_coalition = coalition.side.BLUE,
+        subRange = {
+            {
+                name = "Center SAM",
+                groupsToSpawn = {"SAM SA6 Akamas"},
+                holdFire = false,
+            },
+            {
+                name = "SAM",
+                groupsToSpawn = {"SAM SA2 Akamas"},
+                holdFire = false
+            }
+        }
+    }
+}
