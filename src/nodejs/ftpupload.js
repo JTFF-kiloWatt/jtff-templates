03c1363906635089d5a5ef7945e2e9a1c127e419
Sebastien LONGO (Klanik) <slongo@klanik.com>
2022-11-17 16:47:43 +0100

    fix(ci): again :)

--- src/nodejs/ftpupload.js
+++ src/nodejs/ftpupload.js
@@ -3,4 +3,4 @@ const templatelib = require('@jtff/miztemplate-lib');
 const config = require("../../config.json");
 const jtffci = new templatelib.MizTemplateCI(config);
 
-process.exit(templatelib.ftpupload(jtffci));
+templatelib.ftpupload(jtffci);
