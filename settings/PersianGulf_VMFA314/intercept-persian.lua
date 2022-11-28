11621360b63b04de5e5ae7e0b92e50c8c69b6913
Sebastien LONGO <sebastien.longo@oufy.net>
2022-11-28 14:55:37 +0100

    normalize PG template

---
+++ settings/PersianGulf_VMFA314/intercept-persian.lua
@@ -0,0 +1,57 @@
+InterceptConfig = {
+    {
+        enable = true,
+        name = 'Civilian',
+        benefitCoalition = coalition.side.BLUE,
+        type = 'civil',
+        skill = 'Excellent',
+        templates = {
+            'Intercept-737'
+        }
+    },
+    {
+        enable = true,
+        name = 'Iranian AF',
+        benefitCoalition = coalition.side.BLUE,
+        type = 'fighter',
+        skill = 'Excellent',
+        templates = {
+            'Intercept-mig29',
+            'Intercept-mig21',
+            'Intercept-tomcatA'
+        },
+    },
+    {
+        enable = true,
+        name = 'US AF',
+        benefitCoalition = coalition.side.RED,
+        type = 'fighter',
+        skill = 'Excellent',
+        templates = {
+            'Intercept-F16',
+            'Intercept-tomcat',
+        },
+    },
+    {
+        enable = true,
+        name = 'Russian Bombers',
+        benefitCoalition = coalition.side.BLUE,
+        type = 'fastbomber',
+        skill = 'Excellent',
+        templates = {
+            'Intercept-tu160',
+            'Intercept-tu22'
+        },
+    },
+    {
+        enable = true,
+        name = 'US Bombers',
+        benefitCoalition = coalition.side.RED,
+        type = 'fastbomber',
+        skill = 'Excellent',
+        templates = {
+            'Intercept-B1',
+            'Intercept-B2'
+        },
+    },
+}
--- settings/PersianGulf_VMFA314/tankers-persian.lua
+++ settings/PersianGulf_VMFA314/tankers-persian.lua
@@ -93,4 +93,139 @@ TankersConfig = {
 
 
 OnDemandTankersConfig = {
+    --Shell 3 KC135
+    {
+        enable = true,
+        type = "sh3",
+        benefit_coalition = coalition.side.BLUE,
+        baseUnit = AIRBASE.PersianGulf.Al_Dhafra_AB,
+        terminalType = AIRBASE.TerminalType.OpenBig,
+        groupName = 'SHELL 3-1 #IFF:7367FR',
+        missionmaxduration = 105,
+        altitude = 15000,
+        speed = 230,
+        tacan = {
+            channel = 103,
+            morse = 'SH3',
+        },
+        orbit = {
+            heading = 207,
+            length = 20,
+        },
+        freq = 276.2,
+        modex = 016,
+        callsign = {
+            alias = 'Shell',
+            name = CALLSIGN.Tanker.Shell,
+            number = 3
+        }
+    },
+    --Arco 6 KC135
+    {
+        enable = true,
+        type = "ar6",
+        benefit_coalition = coalition.side.BLUE,
+        baseUnit = AIRBASE.PersianGulf.Al_Dhafra_AB,
+        terminalType = AIRBASE.TerminalType.OpenBig,
+        groupName = 'ARCO 6-1 #IFF:7541FR',
+        missionmaxduration = 135,
+        altitude = 23000,
+        speed = 420,
+        tacan = {
+            channel = 102,
+            morse = 'AR6',
+        },
+        orbit = {
+            heading = 118,
+            length = 40,
+        },
+        freq = 276.2,
+        modex = 015,
+        callsign = {
+            alias = 'Arco',
+            name = CALLSIGN.Tanker.Arco,
+            number = 6
+        }
+    },
+    --Texaco 2 KC135MPRS
+    {
+        enable = true,
+        type = "tx2",
+        benefit_coalition = coalition.side.BLUE,
+        baseUnit = AIRBASE.PersianGulf.Al_Dhafra_AB,
+        terminalType = AIRBASE.TerminalType.OpenBig,
+        groupName = 'TEXACO 2-1 #IFF:4274FR',
+        missionmaxduration = 135,
+        altitude = 25000,
+        speed = 420,
+        tacan = {
+            channel = 101,
+            morse = 'TEX',
+        },
+        orbit = {
+            heading = 090,
+            length = 40,
+        },
+        freq = 317.5,
+        modex = 012,
+        callsign = {
+            alias = 'Texaco',
+            name = CALLSIGN.Tanker.Texaco,
+            number = 2
+        }
+    },
+    --Texaco 3 KC135MPRS
+    {
+        enable = true,
+        type = "tx3",
+        benefit_coalition = coalition.side.BLUE,
+        baseUnit = AIRBASE.PersianGulf.Al_Dhafra_AB,
+        terminalType = AIRBASE.TerminalType.OpenBig,
+        groupName = 'TEXACO 3-1 #IFF:4275FR',
+        missionmaxduration = 135,
+        altitude = 26000,
+        speed = 420,
+        tacan = {
+            channel = 107,
+            morse = 'TX3',
+        },
+        orbit = {
+            heading = 090,
+            length = 30,
+        },
+        freq = 317.5,
+        modex = 23,
+        callsign = {
+            alias = 'Texaco-3',
+            name = CALLSIGN.Tanker.Texaco,
+            number = 3
+        }
+    },
+    --Arco 7 KC135
+    {
+        enable = true,
+        type = "ar7",
+        benefit_coalition = coalition.side.BLUE,
+        baseUnit = AIRBASE.PersianGulf.Al_Dhafra_AB,
+        terminalType = AIRBASE.TerminalType.OpenBig,
+        groupName = 'ARCO 7-1 #IFF:7542FR',
+        missionmaxduration = 135,
+        altitude = 23000,
+        speed = 420,
+        tacan = {
+            channel = 108,
+            morse = 'AR7',
+        },
+        orbit = {
+            heading = 090,
+            length = 30,
+        },
+        freq = 276.2,
+        modex = 029,
+        callsign = {
+            alias = 'Arco-7',
+            name = CALLSIGN.Tanker.Arco,
+            number = 7
+        }
+    },
 }
