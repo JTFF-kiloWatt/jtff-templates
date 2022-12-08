0d95d7e5b91399257a7ba11e49cc4941916c797e
Alexandre PROUST <alexproust@gmail.com>
2022-12-08 20:03:09 +0100

    Few fix on Persian Template
    Carriers speed on return
    Carriers name for DATIS
    Static units on CVN-73
    Mission briefing for AIRBOSS
    Carrier escort waypoint
    Delete SA-11 on Lar

--- settings/PersianGulf/rescue-helos-persian.lua
+++ settings/PersianGulf/rescue-helos-persian.lua
@@ -2,7 +2,7 @@ PedrosConfig = {
     {
         enable = true,
         autorespawn = true,
-        patternUnit = 'washington',
+        patternUnit = 'CARRIER Washington 251.750',
         baseUnit = 'Escort CVN_73 Ticonderoga',
         groupName = 'Pedro-1 #IFF:7325FR',
         freq = 126.6,
@@ -11,7 +11,7 @@ PedrosConfig = {
     {
         enable = true,
         autorespawn = true,
-        patternUnit = 'roosevelt',
+        patternUnit = 'CARRIER Roosevelt 251.850',
         baseUnit = 'Escort CVN_71 Ticonderoga',
         groupName = 'Pedro-2 #IFF:7326FR',
         freq = 127.6,
